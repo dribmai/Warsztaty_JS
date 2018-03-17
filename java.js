cba4f08a8e25aa5e4b5b383285dfeb33eb77b957
Agata <agataduda92@gmail.com>
2018-03-17 15:44:37 +0100

    zmiana1

---
+++ java.js
@@ -0,0 +1,13 @@
+console.log("działa!");
+var myName = 'Agata';
+console.log(myName);
+var imiona = ['Agata','Diana','Bartek'];
+console.log(imiona[1]);
+var a = 5;
+var b = 3;
+if(a>b)
+{console.log("Hurra!")};
+
+for (var i=0;i<=imiona.length-1;i++){
+  console.log(imiona[i]);
+}
